e6a37a45a01636620132c2db1e7b91ce4cfe99f0
Pham Tri Dat <seedam5000@gmail.com>
2025-06-17 07:06:03 +0700

    Cập nhật .gitignore

--- backend/management/src/main/java/com/seedam/library/management/repository/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seedam.library.management.repository;
-
-import com.seedam.library.management.model.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends JpaRepository <Publisher, String> {
-}
